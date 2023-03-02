458f816a79e4d29bccbc398411b6f40c2018a2e4
CGFDEZ01 <124289689+CGFDEZ01@users.noreply.github.com>
2023-03-02 20:53:08 +0100

    .

--- EjerciciosClase/src/ejercicios/Persona.java
+++ EjerciciosClase/src/ejercicios/Persona.java
@@ -4,6 +4,9 @@
  */
 package ejercicios;
 
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 /**
  *
  * @author carlos
@@ -32,7 +35,11 @@ public class Persona {
         this.altura = 0;
     }
     
-    public Persona(String nombre, int edad, String DNI, char sexo, double peso, double altura) {
+    public Persona(String nombre, int edad, String DNI, char sexo, double peso, double altura) throws DNIException {
+        if (!esDNIValido(DNI)) {
+            throw new DNIException("El DNI introducido no es válido.");
+        }
+        
         this.nombre = nombre;
         this.edad = edad;
         this.DNI = DNI;
@@ -41,9 +48,19 @@ public class Persona {
         this.altura = altura;
     }   
     
+    public class IMCException extends Exception {
+        public IMCException(String message) {
+            super(message);
+        }
+    }
+    
     // Métodos
-    public int calcularIMC() {
+    public int calcularIMC() throws IMCException {
+        if (!esIMCvalido(this.peso, this.altura)){
+            throw new IMCException("El peso, la altura o ambos no son válidos para calcular el IMC");
+        }
         double imc = peso / Math.pow(altura, 2);
+        
         if (imc < 20) {
             return -1; // Persona por debajo de su peso ideal
         } else if (imc >= 20 && imc <= 25) {
@@ -53,6 +70,16 @@ public class Persona {
         }
     
     }
+    public boolean esIMCvalido(double peso, double altura){
+        boolean esValido = false;
+        if (peso >= 5 && peso <= 634){
+            if (altura >= 0.5 && altura <= 2.5){
+                esValido = true;
+            }
+        }
+        return esValido;
+    }
+    
     public boolean esMayorDeEdad() {
         return edad >= 18;
     }
@@ -66,12 +93,29 @@ public class Persona {
     }
     
     public String toString() {
-        return "Nombre = "+ nombre +
-                "\nEdad = " + edad +
-                "\nDNI = " + DNI + 
-                "\nSexo = " + sexo +
-                "\nPeso = " + peso +
-                "\nAltura = " + altura;
+     try {
+         return "Nombre = "+ nombre +
+                 "\nEdad = " + edad +
+                 "\nDNI = " + DNI +
+                 "\nSexo = " + sexo +
+                 "\nPeso = " + peso +
+                 "\nAltura = " + altura +
+                 "\nIMC = " + (calcularIMC() == 0 ? calcularIMC() + " la persona tiene un peso normal" : calcularIMC() == 1 ? calcularIMC() + " la persona tiene sobrepeso": calcularIMC() + " la persona tiene un peso nefasto");
+     } catch (IMCException ex) { 
+         Logger.getLogger(Persona.class.getName()).log(Level.SEVERE, null, ex);
+     }
+     return "Nombre = "+ nombre +
+                 "\nEdad = " + edad +
+                 "\nDNI = " + DNI +
+                 "\nSexo = " + sexo +
+                 "\nPeso = " + peso +
+                 "\nAltura = " + altura;
+    }
+    
+    public class DNIException extends Exception {
+        public DNIException(String message) {
+            super(message);
+        }
     }
     
     private void generaDNI() {
@@ -84,6 +128,27 @@ public class Persona {
         
         this.DNI = dniCompleto;
     }
+    
+    // Comprueba si un DNI es válido
+    private boolean esDNIValido(String dni) {
+        final int LONGITUD_DNI = 9; // 8 números y una letra
+        boolean esValido = false;
+
+        // Comprobamos si el DNI tiene la longitud correcta
+        if (dni.length() == LONGITUD_DNI) {
+            // Comprobamos si los 8 primeros caracteres son números
+            String numeros = dni.substring(0, 8);
+            if (numeros.matches("[0-9]+")) {
+                // Comprobamos si el último caracter es una letra
+                String letra = dni.substring(8);
+                if (letra.matches("[A-Za-z]")) {
+                    esValido = true;
+                }
+            }
+        }
+        return esValido;
+    }
+
 
     
     //Métodos set
@@ -105,5 +170,18 @@ public class Persona {
 
     public void setAltura(double altura) {
         this.altura = altura;
+    }
+    
+    public static void main(String[] args){
+        try {
+        Persona p1 = new Persona("Carlos", 21, "54222645F", 'H', 67, 3);
+        System.out.println(p1.toString());
+        System.out.println(p1.calcularIMC());
+    
+        }catch(DNIException e){
+            System.err.println(e.getMessage()); 
+        }catch(IMCException e){
+            System.err.println(e.getMessage());
+        }
     }
 }
